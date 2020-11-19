35de7a9f52e55572bbc36a6569f0d0ac7916cee9
marcin-zajac <57039654+marcin-zajac@users.noreply.github.com>
2020-11-19 16:46:29 +0100

    add MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
 
 app.use(express.json());
 
+// Connect to MongoDB
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
